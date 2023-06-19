6fc91ce11334331b40627a89fccc2e0d3d9266fb
юрбан кустов <kustov.ius@students.dvfu.ru>
2023-06-19 17:38:25 +1000

    add domain function processing data

---
+++ app/src/main/java/com/example/fefu_fitnes_compose/Domain/use_case/dataConverters/isCorrectEvent.kt
@@ -0,0 +1,13 @@
+package com.example.fefu_fitnes_compose.Domain.use_case.dataConverters
+
+import com.example.fefu_fitnes_compose.DataPakage.Models.ServicesModels.UserPlans
+
+class isCorrectEvent {
+    fun execute(eventName:String, plans:UserPlans):Boolean{
+        if(plans.size != 0){
+            for (plan in plans)
+                if (plan.serviceName == eventName) return true
+        }
+        return false
+    }
+}
